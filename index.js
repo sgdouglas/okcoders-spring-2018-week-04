6a3fa00b9beaaa2f3422d6f756c8054d5c5209e9
Sean Douglas <sgdouglas@gmail.com>
2018-03-24 10:59:47 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var $ = require("jquery");
+var _ = require("lodash");
+
+var list = [ "john", "paul", "george", "ringo" ];
+
+function message(){
+	console.log("This is a message");
+	Alert("This is a message");
+}
